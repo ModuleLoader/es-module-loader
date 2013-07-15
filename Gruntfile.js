3b1a11e6ac4c1c98ccd729ab73218ec04d2f8501
guybedford <guybedford@gmail.com>
2013-07-15 11:40:23 -0700

    grunt file update (resolves #15)

---
+++ Gruntfile.js
@@ -0,0 +1,36 @@
+'use strict';
+module.exports = function (grunt) {
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    meta: {
+      banner: '/*!\n*  <%= pkg.name %> - v<%= pkg.version %> - ' +
+        '<%= grunt.template.today("m/d/yyyy") %>' + "\n" +
+        '<%= pkg.homepage ? "*  " + pkg.homepage + "\\n" : "" %>' +
+        '*  Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author.name %>;' +
+        ' Licensed <%= _.pluck(pkg.licenses, "type").join(", ") %>\n*/\n'
+    },
+    uglify: {
+      options: {
+        banner: '<%= meta.banner %>'
+      },
+      dist: {
+          src: 'lib/<%= pkg.name %>.js',
+          dest: 'dist/<%= pkg.name %>.min.js'
+      }
+    },
+    jshint: {
+      options: {
+        jshintrc: '.jshintrc'
+      },
+      dist: [
+        'lib/es6-module-loader.js',
+        'test/es6-module-loader_test.js'
+      ]
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+
+  grunt.registerTask('default', [/*'jshint', */'uglify']);
+};
