2f112995660965a951ff3f000f52fe8ceec4c150
guybedford <guybedford@gmail.com>
2013-08-20 13:58:32 +0200

    normalization fixes for below base url and closer to spec, including tests

--- test/es6-module-loader_test.js
+++ test/es6-module-loader_test.js
@@ -1,6 +1,14 @@
-var ML = require('../lib/es6-module-loader.js');
+var System = require('../lib/es6-module-loader.js').System;
 
-ML.System.import('test-file', function(m) {
+System.import('test-file', function(m) {
   if (m.hello == 'world')
-    console.log('1 test passed');
+    console.log('Basic load passed');
 });
+
+console.log('Normalize test 1: ' + (System.normalize('./a/b', { name: 'c' }) == 'a/b'));
+console.log('Normalize test 2: ' + (System.normalize('./a/b', { name: 'c/d' }) == 'c/a/b'));
+console.log('Normalize test 3: ' + (System.normalize('./a/b', { name: '../c/d' }) == '../c/a/b'));
+console.log('Normalize test 4: ' + (System.normalize('./a/b', { name: '../c/d' }) == '../c/a/b'));
+console.log('Normalize test 5: ' + (System.normalize('../a/b', { name: '../../c/d' }) == '../../a/b'));
+console.log('Normalize test 6: ' + (System.normalize('../../a/b', { name: 'c/d' }) == '../a/b'));
+
