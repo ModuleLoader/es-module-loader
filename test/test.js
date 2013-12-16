8041eaef4eae66dd72b2bc1de0c16ee13a965543
guybedford <guybedford@gmail.com>
2013-12-17 01:46:52 +0200

    rewrite for 2013-12-02 spec

--- test/test.js
+++ test/test.js
@@ -1,30 +1,34 @@
 
-var System, test, tests;
+var System, Loader, Module, tests, test;
 
 var testCnt = 0, passed = 0, failed = 0;
 var test = function(name, initialize) {
+  if (typeof initialize != 'function') {
+    var val = initialize;
+    var exp = arguments[2];
+    initialize = function(assert) {
+      assert(val, exp);
+    }
+  }
   var testId = testCnt++;
   tests.addTest(testId, name);
-  initialize(function() {
-    var failed = false;
-    var failedAssertion;
-    for (var i = 0; i < arguments.length; i++)
-      if (!arguments[i]) {
-        failed = true;
-        failedAssertion = i + 1;
-      }
-    if (!failed)
-      passed++;
+  function assert(value, expected) {
+    if (value != expected)
+      return 'Got "' + value + '" instead of "' + expected + '"';
+  }
+  initialize(function(value, expected) {
+    var failure;
+    if (value instanceof Array) {
+      for (var i = 0; i < arguments.length; i++)
+        failure = failure || assert(arguments[i][0], arguments[i][1]);
+    }
     else
+      failure = assert(value, expected);
+    if (failure)
       failed++;
-    tests.completeTest(testId, name, {
-      passed: !failed, 
-      failedAssertion: failedAssertion
-    }, {
-      passed: passed, 
-      failed: failed, 
-      total: testCnt
-    });
+    else
+      passed++;
+    tests.completeTest(testId, name, failure, { passed: passed, failed: failed, total: testCnt });
   });
 }
 
@@ -37,93 +41,244 @@ if (typeof window != 'undefined') {
       p.innerHTML = '<td>' + name + '</td><td class="result-' + id + '"></td>'
       document.querySelector('.test tbody').appendChild(p);
     },
-    completeTest: function(id, name, result, summary) {
-      document.querySelector('.test .result-' + id).innerHTML = result.passed ? 'Passed' : 'Failed Assertion ' + result.failedAssertion;
+    completeTest: function(id, name, failure, summary) {
+      document.querySelector('.test .result-' + id).innerHTML = !failure ? 'Passed' : 'Failed: ' + failure;
       document.querySelector('.summary').innerHTML = summary.passed + '/' + summary.total + ' tests passed';
     }
   }
+  window.test = test;
+  window.runTests = runTests;
 }
 else {
   // nodejs
-  System = require('../lib/es6-module-loader.js').System;
-  require('traceur') = require('../lib/traceur');
+  var ml = require('../lib/es6-module-loader');
+
+  System = ml.System;
+  Loader = ml.Loader;
+  Module = ml.Module;
 
   tests = {
     addTest: function(id, name) {},
-    completeTest: function(id, name, result, summary) {
-      console.log(name + ': ' + (result.passed ? 'Passed' : 'Failed Assertion ' + result.failedAssertion));
+    completeTest: function(id, name, failure, summary) {
+      console.log(name + ': ' + (!failure ? 'Passed' : 'Failed: ' + result.failedAssertion));
       console.log(summary.passed + '/' + summary.total + ' passed. ');
     },
   };
+
+  runTests();
 }
 
-test('Normalize test 1', function(assert) {
-  assert(System.normalize('./a/b', { name: 'c' }) == 'a/b');
-});
-test('Normalize test 2', function(assert) {
-  assert(System.normalize('./a/b', { name: 'c/d' }) == 'c/a/b');
-});
-test('Normalize test 3', function(assert) {
-  assert(System.normalize('./a/b', { name: '../c/d' }) == '../c/a/b');
-});
-test('Normalize test 4', function(assert) {
-  assert(System.normalize('./a/b', { name: '../c/d' }) == '../c/a/b');
-});
-test('Normalize test 5', function(assert) {
-  assert(System.normalize('../a/b', { name: '../../c/d' }) == '../../a/b');
-});
-test('Normalize test 6', function(assert) {
-  assert(System.normalize('../../a/b', { name: 'c/d' }) == '../a/b');
-});
-test('Normalize test 7', function(assert) {
-  assert(System.normalize('../core', { name: 'core/ready' }) == 'core');
-});
+function runTests() {
+  // Normalize tests - identical to https://github.com/google/traceur-compiler/blob/master/test/unit/runtime/System.js
 
-test('Export Syntax', function(assert) {
-  System.import('syntax/export', function(m) {
-    assert(
-      m.p === 5,
-      typeof m.foo == 'function',
-      typeof m.q == 'object',
-      m.default.name == 'bar',
-      m.s == 4,
-      m.t == 4,
-      typeof m.m == 'object'
-    );
+  var oldBaseURL = System.baseURL;
+  System.baseURL = 'http://example.org/a/b.html';
+
+  test('Normalize - No Referer', System.normalize('d/e/f'), 'd/e/f');
+  // test('Normalize - Below baseURL', System.normalize('../e/f'), '../e/f');
+
+  var refererName = 'dir/file';
+  test('Normalize - Relative paths', System.normalize('./d/e/f', refererName), 'dir/d/e/f');
+  test('Normalize - Relative paths', System.normalize('../e/f', refererName), 'e/f');
+
+  test('Normalize - name undefined', function(assert) {
+    try {
+      System.normalize(undefined, refererName);
+    }
+    catch(e) {
+      assert(e.message, 'Module name must be a string');
+    }
   });
-});
 
-test('Re-export', function(assert) {
-  System.import('syntax/reexport1', function(m) {
-    assert(m.p === 5);
+  test('Normalize - embedded ..', function(assert) {
+    try {
+      System.normalize('a/b/../c');
+    }
+    catch(e) {
+      assert(e.message, 'Illegal module name"a/b/../c"');
+    }
   });
-});
-
-test('Re-export with new name', function(assert) {
-  System.import('syntax/reexport2', function(m) {
-    assert(m.q == 4, m.z == 5);
-  })
-});
-
-test('Import Syntax', function(assert) {
-  System.import('syntax/import', function(m) {
-    assert(
-      m.a.name == 'bar',
-      m.b == 4,
-      m.c == 5,
-      m.d == 4,
-      m.q == System.get('syntax/export')
-    );
+  test('Normalize - embedded ..', function(assert) {
+    try {
+      System.normalize('a/../b', refererName);
+    }
+    catch(e) {
+      assert(e.message, 'Illegal module name"a/../b"');
+    }
   });
-});
-
-test('ES6 Syntax', function(assert) {
-  System.import('syntax/es6-file', function(m) {
-    assert(m);
+  test('Normalize - embedded ..', function(assert) {
+    try {
+      System.normalize('a/b/../c', refererName);
+    }
+    catch(e) {
+      assert(e.message, 'Illegal module name"a/b/../c"');
+    }
   });
-});
+
+  // test('Normalize - below referer', System.normalize('../../e/f', refererName), '../e/f');
+
+  test('Normalize - backwards compat', System.normalize('./a.js'), 'a.js');
+
+  /*
+  test('Normalize - URL', function(assert) {
+    try {
+      assert(System.normalize('http://example.org/a/b.html'), 'http://example.org/a/b.html');
+    }
+    catch(e) {
+      assert(e, 'http://example.org/a/b.html');
+    }
+  });
+
+  test('Normalize - Canonicalize URL', function(assert) {
+    try {
+      assert(System.normalize('http://example.org/a/../b.html'), 'http://example.org/b.html');
+    }
+    catch(e) {
+      assert(e, 'http://example.org/b.html')
+    }
+  });
+  */
+
+  System.baseURL = 'http://example.org/a/';
+
+  test('Locate', System.locate({ name: '@abc/def' }), 'http://example.org/a/@abc/def.js');
+  test('Locate', System.locate({ name: 'abc/def' }), 'http://example.org/a/abc/def.js');
+
+  System.baseURL = oldBaseURL;
 
 
 
+  // More Normalize tests
+
+  test('Normalize test 1', function(assert) {
+    assert(System.normalize('./a/b', 'c'), 'a/b');
+  });
+  test('Normalize test 2', function(assert) {
+    assert(System.normalize('./a/b', 'c/d'), 'c/a/b');
+  });
+  test('Normalize test 3', function(assert) {
+    assert(System.normalize('./a/b', '../c/d'), '../c/a/b');
+  });
+  test('Normalize test 4', function(assert) {
+    assert(System.normalize('./a/b', '../c/d'), '../c/a/b');
+  });
+  test('Normalize test 5', function(assert) {
+    assert(System.normalize('../a/b', '../../c/d'), '../../a/b');
+  });
 
 
+  test('Import a script', function(assert) {
+    System.import('syntax/script').then(function(m) {
+      assert(!!m, true);
+    });
+  });
+
+  test('Import ES6', function(assert) {
+    System.import('syntax/es6').then(function(m) {
+      assert(m.p, 'p');
+    });
+  });
+
+  test('Import ES6 with dep', function(assert) {
+    System.import('syntax/es6-withdep').then(function(m) {
+      assert(m.p, 'p');
+    });
+  });
+
+  test('Export Syntax', function(assert) {
+    System.import('syntax/export').then(function(m) {
+      assert(
+        [m.p, 5],
+        [typeof m.foo, 'function'],
+        [typeof m.q, 'object'],
+        [m.default.name, 'bar'],
+        [m.s, 4],
+        [m.t, 4],
+        [typeof m.m, 'object']
+      );
+    });
+  });
+
+  test('Re-export', function(assert) {
+    System.import('syntax/reexport1').then(function(m) {
+      assert(m.p, 5);
+    });
+  });
+
+  test('Re-export with new name', function(assert) {
+    System.import('syntax/reexport2').then(function(m) {
+      assert(
+        [m.q, 4],
+        [m.z, 5]
+      );
+    }, function(err) {
+      console.log('error');
+      console.log(err);
+    });
+  });
+
+  test('Import Syntax', function(assert) {
+    System.import('syntax/import').then(function(m) {
+      assert(
+        [m.a.name, 'bar'],
+        [m.b, 4],
+        [m.c, 5],
+        [m.d, 4],
+        [typeof m.q.foo, 'function']
+      );
+    });
+  });
+
+
+  test('ES6 Syntax', function(assert) {
+    System.import('syntax/es6-file').then(function(m) {
+      assert(typeof m.q, 'function');
+    });
+  });
+
+
+  var customLoader = new Loader({
+    normalize: System.normalize,
+    locate: function(load) {
+      if (load.name.substr(0, 5) == 'path/')
+        load.name = 'loader/' + load.name.substr(5);
+      return System.locate(load);
+    },
+    fetch: System.fetch,
+    translate: System.translate,
+    instantiate: function(load) {
+      // very bad AMD support
+      if (load.source.indexOf('define') == -1)
+        return System.instantiate(load);
+      
+      var factory, deps;
+      var define = function(_deps, _factory) {
+        deps = _deps;
+        factory = _factory;
+      }
+      eval(load.source);
+      return {
+        deps: deps,
+        execute: function() {
+          return new Module(factory.apply(null, arguments));
+        }
+      }
+    }
+  });
+
+  test('Custom loader standard load', function(assert) {
+    customLoader.import('loader/test').then(function(m) {
+      assert(m.loader, 'custom');
+    });
+  });
+  test('Custom loader special rules', function(assert) {
+    customLoader.import('path/custom').then(function(m) {
+      assert(m.path, true);
+    });
+  });
+  test('Custom loader AMD support', function(assert) {
+    customLoader.import('loader/amd').then(function(m) {
+      assert(m.format, 'amd');
+    });
+  });
+}
