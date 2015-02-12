606f8e5351061be7d3cf7be25dfcf070fda64b21
guybedford <guybedford@gmail.com>
2015-02-12 16:08:52 +0200

    0.13.1

--- dist/es6-module-loader.js
+++ dist/es6-module-loader.js
@@ -1,5 +1,5 @@
 /*
- *  es6-module-loader v0.13.0
+ *  es6-module-loader v0.13.1
  *  https://github.com/ModuleLoader/es6-module-loader
  *  Copyright (c) 2015 Guy Bedford, Luke Hoban, Addy Osmani; Licensed MIT
  */
