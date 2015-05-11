a0ee0f4f8c7e50843a2fcabe0fc5e3a6508e0875
guybedford <guybedford@gmail.com>
2015-05-12 01:19:14 +0200

    Windows Node paths fix

--- index.js
+++ index.js
@@ -3,16 +3,18 @@ if (typeof Promise === 'undefined')
 
 var System = require('./dist/es6-module-loader-dev.src');
 
+var filePrefix = 'file:' + (process.platform.match(/^win/) ? '/' : '') + '//';
+
 try {
-  System.paths.traceur = 'file:' + require.resolve('traceur/bin/traceur.js');
+  System.paths.traceur = filePrefix + require.resolve('traceur/bin/traceur.js');
 }
 catch(e) {}
 try {
-  System.paths.babel = 'file:' + require.resolve('babel-core/browser.js');
+  System.paths.babel = filePrefix + require.resolve('babel-core/browser.js');
 }
 catch(e) {}
 try {
-  System.paths.babel = System.paths.babel || 'file:' + require.resolve('babel/browser.js');
+  System.paths.babel = System.paths.babel || filePrefix + require.resolve('babel/browser.js');
 }
 catch(e) {}
 
