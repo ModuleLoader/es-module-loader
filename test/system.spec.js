673d16034393bec2101fb61f448ec3106d6d6646
guybedford <guybedford@gmail.com>
2015-06-22 22:04:39 +0200

    fix up tests

--- test/system.spec.js
+++ test/system.spec.js
@@ -227,7 +227,7 @@ describe('System', function () {
         System.import('test/loads/load-non-existent.js')
           .then(supposedToFail)
           .catch(function (e) {
-            expect(e).to.be.match(/Error loading \S+/);
+            expect(typeof window != 'undefined' ? e.toString() : e.stack).to.be.match(/Error loading \S+/);
           })
           .then(done, done);
       });
