a6ee774876039de34aca8b2a9c381de0e8f142dc
guybedford <guybedford@gmail.com>
2015-07-23 13:10:06 +0200

    improve Node file error

--- src/system-fetch.js
+++ src/system-fetch.js
@@ -59,7 +59,7 @@
     var fs;
     fetchTextFromURL = function(url, fulfill, reject) {
       if (url.substr(0, 8) != 'file:///')
-        throw 'Only file URLs of the form file:/// allowed running in Node.';
+        throw new Error('Unable to fetch "' + url + '". Only file URLs of the form file:/// allowed running in Node.');
       fs = fs || require('fs');
       if (isWindows)
         url = url.replace(/\//g, '\\').substr(8);
