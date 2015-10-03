e47754bc76412062b3036aadd9bdbc7e9e4e5d3a
Joel Denning <joeldenning@gmail.com>
2015-10-03 11:13:34 -0600

    Updating numbering throughout, and rearranging code so the numbers are sequential

--- src/loader.js
+++ src/loader.js
@@ -23,7 +23,6 @@
     };
   }
 
-  // 3.1 Module Registry
   // States
   var FETCH = 0;
   var TRANSLATE = 1;
@@ -32,9 +31,195 @@
   var LINK = 4;
   var READY = 5;
 
-  // 4. Loading
+  // 3.3.2
+  Loader.prototype['import'] = function(name, referrer) {
+    var loader = this._loader;
+    var metadata = {};
+    return Promise.resolve()
+    .then(function() {
+      return loader.resolve.call(loader.loaderObj, name, referrer, metadata);
+    })
+    ['catch'](function(err) {
+      throw addToError(err, 'Resolving ' + name + (referrer ? ', ' + referrer : ''));
+    })
+    .then(function(key) {
+      return requestReady(loader, key, metadata);
+    });
+  };
 
-  // 4.1.1
+  // 3.3.3
+  Loader.prototype.resolve = function(name, referrer, metadata) {
+    var loader = this._loader;
+    return loader.resolve.call(loader.loaderObj, name, referrer, metadata || {});
+  };
+
+  // 3.3.4
+  // For eg ready, <script type="module" src="${key}"></script>
+  Loader.prototype.load = function(key, stage, metadata) {
+    var loader = this._loader;
+    
+    if (stage == 'fetch')
+      return requestFetch(loader, key, metadata);
+    
+    else if (stage == 'translate')
+      return requestTranslate(loader, key, metadata);
+    
+    else if (stage == 'instantiate')
+      return requestInstantiateAll(loader, key, metadata)
+      .then(function(entry) {
+        if (!(entry.module instanceof Module))
+          return entry.module;
+      });
+    
+    else if (stage == 'link')
+      return requestLink(loader, key, metadata).then(function() {})
+    
+    else if (!stage || stage == 'ready')
+      return requestReady(loader, key, metadata)
+      .then(function(entry) {
+        // this is ok because we use plain modules throughout
+        return entry.module;
+      });
+
+    else
+      throw new TypeError('Invalid stage ' + stage);
+  };
+
+  // No longer in spec
+  // For eg fetch, <script type="module">${value}</script>, key = anon
+  Loader.prototype.provide = function(key, stage, value, metadata) {
+    var loader = this._loader;
+
+    var entry = ensureRegistered(loader, key, metadata);
+
+    if (stage == 'fetch') {
+      if (entry.state > FETCH)
+        throw new TypeError(key + ' has already been fetched.');
+      fulfillFetch(loader, entry, value);
+    }
+    else if (stage == 'translate') {
+      if (entry.state > TRANSLATE)
+        throw new TypeError(key + ' has already been translated.');
+      fulfillTranslate(loader, entry, value);
+    }
+    else if (stage == 'instantiate') {
+      if (entry.state > INSTANTIATE)
+        throw new TypeError(key + ' has already been instantiated.');
+      fulfillFetch(loader, entry, undefined);
+      fulfillTranslate(loader, entry, undefined);
+      // NB error propogation
+      entry.translate.then(function(source) {
+        loadTranspilerThenFulfillInstantiate(loader, entry, value, source);
+      });
+    }
+    else
+      throw new TypeError('Invalid stage ' + stage);
+  };
+
+  // TODO: This has been moved to Registry.prototype.lookup (4.4.3)
+  Loader.prototype.lookup = function(key) {
+    var loader = this._loader;
+
+    var entry = loader.registry[key];
+    if (!entry)
+      return null;
+
+    var state;
+    if (entry.state == FETCH)
+      state = 'fetch';
+    else if (entry.state == TRANSLATE)
+      state = 'translate';
+    else if (entry.state == INSTANTIATE)
+      state = 'instantiate';
+    else if (entry.state == LINK)
+      state = 'link';
+    else if (entry.state == READY)
+      state = 'ready';
+
+    return {
+      state: state,
+      metadata: entry.metadata,
+      fetch: entry.fetch && Promise.resolve(entry.fetch),
+      translate: entry.translate && Promise.resolve(entry.translate),
+      instantiate: entry.instantiate && Promise.resolve(entry.instantiate),
+      module: entry.state == READY && (entry.module instanceof Module ? entry.module : entry.module.module),
+      error: entry.error
+    };
+  };
+
+  // TODO: this has been moved to Registry.prototype.install (4.4.4)
+  Loader.prototype.install = function(key, module) {
+    var loader = this._loader;
+
+    if (loader.registry[key])
+      throw new TypeError(key + ' is already defined in the Loader registry.');
+
+    if (!(module instanceof Module))
+      throw new TypeError('Install must provide a valid Module object.');
+
+    loader.registry[key] = {
+      key: key,
+      state: READY,
+      metadata: metadata,
+
+      fetch: undefined,
+      translate: undefined,
+      instantiate: undefined,
+
+      dependencies: undefined,
+      module: module,
+      declare: undefined,
+      error: null
+    };
+  };
+
+  // TODO: this has been moved to Registry.prototype.uninstall (4.4.5)
+  Loader.prototype.uninstall = function(key) {
+    var loader = this._loader;
+
+    var entry = loader.registry[key];
+    if (!entry)
+      throw new TypeError(key + ' is not defined in the Loader registry.');
+
+    if (entry.state < LINK)
+      throw new TypeError(key + ' is still loading.');
+
+    delete loader.registry[key];
+  };
+
+  // TODO: this has been moved to Registry.prototype.cancel (4.4.6)
+  Loader.prototype.cancel = function(key) {
+    var loader = this._loader;
+
+    var entry = loader.registry[key];
+    if (!entry)
+      throw new TypeError(key + ' does not exist.');
+
+    if (entry.state >= LINK)
+      throw new TypeError(key + ' is already past linking.');
+
+    delete loader.registry[key];
+  };
+
+  // TODO: the Loader no longer has the hook property
+  // loader.hook('resolve') -> returns resolve hook
+  // loader.hook('resolve', fn) -> sets resolve hook
+  var hooks = ['resolve', 'fetch', 'translate', 'instantiate'];
+  Loader.prototype.hook = function(name, value) {
+    var loader = this._loader;
+    if (indexOf.call(hooks, name) == -1)
+      throw new TypeError(name + ' is not a valid hook.');
+    if (value)
+      loader[name] = value;
+    else
+      return loader[name];
+  };
+
+  // 4. Registry Objects
+
+  // 5. Loading
+
+  // 5.1.1
   function ensureRegistered(loader, key, metadata) {
     return loader.registry[key] || (loader.registry[key] = {
       key: key,
@@ -59,9 +244,9 @@
     });
   }
 
-  // 4.1.2 inlined
+  // 5.1.2 inlined
 
-  // 4.1.3
+  // 5.1.3
   function fulfillFetch(loader, entry, payload) {
     if (entry.fetchResolve)
       entry.fetchResolve(payload);
@@ -72,7 +257,7 @@
     entry.state = Math.max(entry.state, TRANSLATE);
   }
 
-  // 4.1.4
+  // 5.1.4
   function fulfillTranslate(loader, entry, source) {
     if (entry.translateResolve)
       entry.translateResolve(source);
@@ -83,11 +268,11 @@
     entry.state = Math.max(entry.state, INSTANTIATE);
   }
 
-  // 4.1.5
+  // 5.1.5
   function fulfillInstantiate(loader, entry, instance, source) {
-    // 4.1.6 CommitInstantiated inlined
+    // 5.1.6 CommitInstantiated inlined
 
-    // 4.1.7 Instantiation inlined
+    // 5.1.7 Instantiation inlined
       if (instance === undefined)
         // defined in transpiler.js
         var registration = transpile(loader.loaderObj, entry.key, source, entry.metadata);
@@ -128,7 +313,7 @@
     });
   }
 
-  // 4.2.1
+  // 5.2.1
   function requestFetch(loader, key, metadata, entry) {
     entry = entry || ensureRegistered(loader, key, metadata);
 
@@ -164,7 +349,7 @@
     });
   }
 
-  // 4.2.2
+  // 5.2.2
   function requestTranslate(loader, key, metadata, entry) {
     entry = entry || ensureRegistered(loader, key, metadata);
 
@@ -203,7 +388,7 @@
     });
   }
 
-  // 4.2.3
+  // 5.2.3
   function requestInstantiate(loader, key, metadata, entry) {
     entry = entry || ensureRegistered(loader, key, metadata);
     
@@ -241,7 +426,7 @@
     });
   }
 
-  // 4.2.4
+  // 5.2.4
   function requestInstantiateAll(loader, key, metadata, entry) {
     entry = entry || ensureRegistered(loader, key, metadata);
 
@@ -274,7 +459,7 @@
     });
   }
 
-  // 4.2.5
+  // 5.2.5
   function requestLink(loader, key, metadata, entry) {
     entry = entry || ensureRegistered(loader, key, metadata);
 
@@ -318,7 +503,7 @@
     });
   }
 
-  // 4.2.6
+  // 5.2.6
   function requestReady(loader, key, metadata, entry) {
     entry = entry || ensureRegistered(loader, key, metadata);
 
@@ -344,12 +529,12 @@
     });
   }
 
-  // 5. Linking
+  // 6. Linking
 
-  // 5.2.1 inlined in 4.2.5
-  // 5.2.2 inlined in 4.2.5
+  // 6.2.1 inlined in 5.2.5
+  // 6.2.2 inlined in 5.2.5
 
-  // 5.2.3
+  // 6.2.3
   function computeDependencyGraph(entry, result) {
     if (indexOf.call(result, entry) != -1)
       return;
@@ -373,197 +558,10 @@
     }
   }
 
-  // 6. API
 
-  // 6.1.1
-  Loader.prototype['import'] = function(name, referrer) {
-    var loader = this._loader;
-    var metadata = {};
-    return Promise.resolve()
-    .then(function() {
-      return loader.resolve.call(loader.loaderObj, name, referrer, metadata);
-    })
-    ['catch'](function(err) {
-      throw addToError(err, 'Resolving ' + name + (referrer ? ', ' + referrer : ''));
-    })
-    .then(function(key) {
-      return requestReady(loader, key, metadata);
-    });
-  };
+  // 7. Module Objects
 
-  // 6.2.1
-  Loader.prototype.resolve = function(name, referrer, metadata) {
-    var loader = this._loader;
-    return loader.resolve.call(loader.loaderObj, name, referrer, metadata || {});
-  };
-
-  // 6.3.1
-  // For eg ready, <script type="module" src="${key}"></script>
-  Loader.prototype.load = function(key, stage, metadata) {
-    var loader = this._loader;
-    
-    if (stage == 'fetch')
-      return requestFetch(loader, key, metadata);
-    
-    else if (stage == 'translate')
-      return requestTranslate(loader, key, metadata);
-    
-    else if (stage == 'instantiate')
-      return requestInstantiateAll(loader, key, metadata)
-      .then(function(entry) {
-        if (!(entry.module instanceof Module))
-          return entry.module;
-      });
-    
-    else if (stage == 'link')
-      return requestLink(loader, key, metadata).then(function() {})
-    
-    else if (!stage || stage == 'ready')
-      return requestReady(loader, key, metadata)
-      .then(function(entry) {
-        // this is ok because we use plain modules throughout
-        return entry.module;
-      });
-
-    else
-      throw new TypeError('Invalid stage ' + stage);
-  };
-
-  // 6.4.1
-  // For eg fetch, <script type="module">${value}</script>, key = anon
-  Loader.prototype.provide = function(key, stage, value, metadata) {
-    var loader = this._loader;
-
-    var entry = ensureRegistered(loader, key, metadata);
-
-    if (stage == 'fetch') {
-      if (entry.state > FETCH)
-        throw new TypeError(key + ' has already been fetched.');
-      fulfillFetch(loader, entry, value);
-    }
-    else if (stage == 'translate') {
-      if (entry.state > TRANSLATE)
-        throw new TypeError(key + ' has already been translated.');
-      fulfillTranslate(loader, entry, value);
-    }
-    else if (stage == 'instantiate') {
-      if (entry.state > INSTANTIATE)
-        throw new TypeError(key + ' has already been instantiated.');
-      fulfillFetch(loader, entry, undefined);
-      fulfillTranslate(loader, entry, undefined);
-      // NB error propogation
-      entry.translate.then(function(source) {
-        loadTranspilerThenFulfillInstantiate(loader, entry, value, source);
-      });
-    }
-    else
-      throw new TypeError('Invalid stage ' + stage);
-  };
-
-  // 6.4.2
-  // SPEC TODO
-  Loader.prototype.error = function(key, stage, value) {};
-
-  // 6.5.1
-  Loader.prototype.lookup = function(key) {
-    var loader = this._loader;
-
-    var entry = loader.registry[key];
-    if (!entry)
-      return null;
-
-    var state;
-    if (entry.state == FETCH)
-      state = 'fetch';
-    else if (entry.state == TRANSLATE)
-      state = 'translate';
-    else if (entry.state == INSTANTIATE)
-      state = 'instantiate';
-    else if (entry.state == LINK)
-      state = 'link';
-    else if (entry.state == READY)
-      state = 'ready';
-
-    return {
-      state: state,
-      metadata: entry.metadata,
-      fetch: entry.fetch && Promise.resolve(entry.fetch),
-      translate: entry.translate && Promise.resolve(entry.translate),
-      instantiate: entry.instantiate && Promise.resolve(entry.instantiate),
-      module: entry.state == READY && (entry.module instanceof Module ? entry.module : entry.module.module),
-      error: entry.error
-    };
-  };
-
-  // 6.5.2
-  Loader.prototype.install = function(key, module) {
-    var loader = this._loader;
-
-    if (loader.registry[key])
-      throw new TypeError(key + ' is already defined in the Loader registry.');
-
-    if (!(module instanceof Module))
-      throw new TypeError('Install must provide a valid Module object.');
-
-    loader.registry[key] = {
-      key: key,
-      state: READY,
-      metadata: metadata,
-
-      fetch: undefined,
-      translate: undefined,
-      instantiate: undefined,
-
-      dependencies: undefined,
-      module: module,
-      declare: undefined,
-      error: null
-    };
-  };
-
-  // 6.5.3
-  Loader.prototype.uninstall = function(key) {
-    var loader = this._loader;
-
-    var entry = loader.registry[key];
-    if (!entry)
-      throw new TypeError(key + ' is not defined in the Loader registry.');
-
-    if (entry.state < LINK)
-      throw new TypeError(key + ' is still loading.');
-
-    delete loader.registry[key];
-  };
-
-  // 6.5.4
-  Loader.prototype.cancel = function(key) {
-    var loader = this._loader;
-
-    var entry = loader.registry[key];
-    if (!entry)
-      throw new TypeError(key + ' does not exist.');
-
-    if (entry.state >= LINK)
-      throw new TypeError(key + ' is already past linking.');
-
-    delete loader.registry[key];
-  };
-
-  // 6.6.1
-  // loader.hook('resolve') -> returns resolve hook
-  // loader.hook('resolve', fn) -> sets resolve hook
-  var hooks = ['resolve', 'fetch', 'translate', 'instantiate'];
-  Loader.prototype.hook = function(name, value) {
-    var loader = this._loader;
-    if (indexOf.call(hooks, name) == -1)
-      throw new TypeError(name + ' is not a valid hook.');
-    if (value)
-      loader[name] = value;
-    else
-      return loader[name];
-  };
-
-  // 6.7 Module Reflection
+  // 7.3 Module Reflection
 
   // plain user-facing module object
   function Module(descriptors, executor, evaluate) {
