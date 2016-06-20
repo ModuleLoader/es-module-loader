813b88428ff0cbe0d4836c310a76c0a36dd50a53
Peter Robins <github@peterrobins.co.uk>
2016-06-20 10:44:09 +0000

    0.50: change to new globals

--- src/wrapper-end.js
+++ src/wrapper-end.js
@@ -3,25 +3,24 @@
     
   var Reflect;
 
-  System = new SystemLoader();
-  System.constructor = SystemLoader;
+  var loader = new SystemLoader();
+  loader.constructor = SystemLoader;
 
-  (function(exports) {
+  if (cjsMode) {
+    exports.Loader = Loader;
+    exports.Module = Module;
+    exports.SystemLoader = SystemLoader;
+  }
 
-    Reflect = exports.Reflect || {};
+  __global.LoaderPolyfill = Loader;
+  __global.ModulePolyfill = Module;
 
-    Reflect.Loader = Reflect.Loader || Loader;
-    Reflect.Module = Reflect.Module || Module;
-    Reflect.global = Reflect.global || __global;
+  __global.Reflect = __global.Reflect || {};
+  __global.Reflect.Module = __global.Reflect.Module || Module;
+  __global.Reflect.Loader = __global.Reflect.Loader || Loader;
 
-    exports.LoaderPolyfill = Loader;
-    exports.ModulePolyfill = Module;
-    exports.Reflect = Reflect;
-    exports.System = System;
-    exports.Registry = Registry;
-
-  })(cjsMode ? exports : __global);
-
-  //module.exports = exports;
+  __global.System = __global.System || {};
+  __global.System.global = __global.System.global || __global;
+  __global.System.loader = __global.System.loader || loader;
 
 })(typeof window != 'undefined' ? window : (typeof WorkerGlobalScope != 'undefined' ? self : global));
