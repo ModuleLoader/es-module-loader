badf8833abcaa7f8b1639b10b2765eed50848033
guybedford <guybedford@gmail.com>
2016-08-10 18:50:29 +0200

    update node loader caveats

--- loader-node.js
+++ loader-node.js
@@ -13,9 +13,14 @@ import { nodeFetch } from './core/fetch.js';
  * Follows the NodeJS resolution algorithm, loading modules first as CJS and then falling back to ES
  * on failure. This effectively provides the "export {}" assumption to load an ES module.
  *
- * Does not currently support the "module" package.json proposal described in the second paragraph at 
+ * - Does not currently support the "module" package.json proposal described in the second paragraph at 
  *   https://github.com/nodejs/node-eps/blob/master/002-es6-modules.md#51-determining-if-source-is-an-es-module
- * Does not allow any loading of ES modules from within CommonJS itself
+ * - Does not allow any loading of ES modules from within CommonJS itself
+ * - Does not implement global require filtering described in 
+ *   https://github.com/nodejs/node-eps/blob/master/002-es6-modules.md#521-removal-of-non-local-dependencies
+ * - Does not provide CJS exports as named exports, skipping the algorithm defined in
+ *   https://github.com/nodejs/node-eps/blob/master/002-es6-modules.md#311-dynamicmodulecreateo
+ *   this may change, pending Node implementation intentions
  * 
  * Alternative Babel options can be set with a local .babelrc file
  */
